70df2c3b51f83a6489c6409c1eac4cd2acf59f1f
Bhokai Dibbavaddha <abaddon@ABADDON.local>
2025-07-26 23:13:08 +0700

    update index

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1753545504|843478537';
+const CACHE_VERSION = '1753546294|1633329282';
 /** @type {string} */
 const CACHE_PREFIX = 'Slap the Leader-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
